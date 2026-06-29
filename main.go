088df021df87928c0d68f9c383fc8b10e845b8a2
MoisesASantos <moisessantosa23@gmail.com>
2026-06-29 16:23:08 +0100

    start doing my request and response handler

--- main.go
+++ main.go
@@ -6,19 +6,26 @@ import (
 	"os"
 )
 
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
-
-
 	mux := http.NewServeMux()
 
+	const filepathRoot = "."
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", readinessHandler)
+
 	server := http.Server{
-		Addr: ":8080",
+		Addr:    ":8080",
 		Handler: mux,
 	}
 
 	err := server.ListenAndServe()
-	defer server.Close()
-	if err != nil {
+	if err != nil && err != http.ErrServerClosed {
 		fmt.Printf("Error on server: %v", err)
 		os.Exit(1)
 	}
