bb4bfb6b116922e83710962b10bbe232ff633177
MoisesASantos <moisessantosa23@gmail.com>
2026-06-30 13:54:15 +0100

    add a middleware function to envolve handler calls, and increment a atomic value when fileserver handler has called

--- main.go
+++ main.go
@@ -4,7 +4,20 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -12,12 +25,27 @@ func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func (cfg *apiConfig) writeHitRequest(w http.ResponseWriter, r *http.Request) {
+
+	body := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+	w.Write([]byte(body))
+}
+
+func (cfg *apiConfig) resetHitRequest(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
+
+
 func main() {
+
+	apiconfig := apiConfig{}
 	mux := http.NewServeMux()
 
 	const filepathRoot = "."
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	mux.HandleFunc("/healthz", readinessHandler)
+	mux.Handle("/app/", apiconfig.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("GET /healthz", readinessHandler)
+	mux.HandleFunc("GET /metrics", apiconfig.writeHitRequest)
+	mux.HandleFunc("POST /reset", apiconfig.resetHitRequest)
 
 	server := http.Server{
 		Addr:    ":8080",
