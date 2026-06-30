b8ff61a9ae533105a07db9ec1a2b2155febc3155
MoisesASantos <moisessantosa23@gmail.com>
2026-06-30 16:18:19 +0100

    add /api/validate_chirp resource for a json packet transmit

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"sync/atomic"
+	"encoding/json"
 )
 
 type apiConfig struct {
@@ -46,6 +47,74 @@ func (cfg *apiConfig) resetHitRequest(w http.ResponseWriter, r *http.Request) {
 }
 
 
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+
+	type returnVals struct {
+        Error string `json:"error"`
+    }
+    
+	respBody := returnVals{
+        Error: msg,
+    }
+
+    dat, err := json.Marshal(respBody)
+	
+	if err != nil {
+			fmt.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+	}
+    
+	w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(code)
+    w.Write(dat)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int) {
+	
+	type returnVals struct {
+        Valid bool `json:"valid"`
+    }
+    
+	respBody := returnVals{
+        Valid: true,
+    }
+
+    dat, err := json.Marshal(respBody)
+	
+	if err != nil {
+			fmt.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+	}
+    
+	w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(code)
+    w.Write(dat)
+}
+
+func chirpRequestHandler(w http.ResponseWriter, r *http.Request) {
+	
+	type parameters struct {
+        Body string `json:"body"`
+    }
+
+    decoder := json.NewDecoder(r.Body)
+    params := parameters{}
+    err := decoder.Decode(&params)
+    if err != nil {
+		fmt.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return
+    }
+
+	if len(params.Body) <= 140 {
+		respondWithJSON(w, 200)
+	} else {
+		respondWithError(w, 400, "Chirp is too long")
+	}
+}
+
 func main() {
 
 	apiconfig := apiConfig{}
@@ -56,6 +125,7 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", readinessHandler)
 	mux.HandleFunc("GET /admin/metrics", apiconfig.writeHitRequest)
 	mux.HandleFunc("POST /admin/reset", apiconfig.resetHitRequest)
+	mux.HandleFunc("POST /api/validate_chirp", chirpRequestHandler)
 
 	server := http.Server{
 		Addr:    ":8080",
